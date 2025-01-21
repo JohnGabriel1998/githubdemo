3f9b4c55cff71e36dadec3a7af98de8748de553c
johncaganda0 <johncaganda0@gmail.com>
2025-01-21 10:48:54 +0900

    commit A

--- mycode.py
+++ mycode.py
@@ -3,6 +3,5 @@ print("my second line")
 print("the japan team made the change once again")
 
 print("changes for second commit");
-print("changes for third commit");
-print("changes for fourth commit");
-print("changes for fifth commit");
+
+print("commit A");
